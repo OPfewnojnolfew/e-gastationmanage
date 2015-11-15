3a8fbbe3fb904d764a9ea85de647af05cb3bcf5d
ace <343656921@qq.com>
2015-11-15 17:31:02 +0800

    fix

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,24 +1,4 @@
 $(function() {
-    Date.prototype.format = function(fmt) {
-        var o = {
-            'M+': this.getMonth() + 1, //月份 
-            'd+': this.getDate(), //日 
-            'h+': this.getHours(), //小时 
-            'm+': this.getMinutes(), //分 
-            's+': this.getSeconds(), //秒 
-            'q+': Math.floor((this.getMonth() + 3) / 3), //季度 
-            'S': this.getMilliseconds() //毫秒 
-        };
-        if (/(y+)/.test(fmt)) {
-            fmt = fmt.replace(RegExp.$1, (this.getFullYear() + '').substr(4 - RegExp.$1.length));
-        }
-        for (var k in o) {
-            if (new RegExp('(' + k + ')').test(fmt)) {
-                fmt = fmt.replace(RegExp.$1, (RegExp.$1.length === 1) ? (o[k]) : (('00' + o[k]).substr(('' + o[k]).length)));
-            }
-        }
-        return fmt;
-    };
     var consumeGrowth = echarts.init($('.J_consumegrowth')[0]),
         payGrowth = echarts.init($('.J_paygrowth')[0]);
     $(window).on('resize', function() {
@@ -61,26 +41,49 @@ $(function() {
             }]
         };
     };
-
-    var _simulationData = function() {
-        var now = new Date(),
-            randomNum = function(begin, end) {
-                begin = Number(begin);
-                end = Number(end);
-                var r = Math.random();
-                if (isNaN(begin) || isNaN(end) || begin > end) {
-                    return r;
-                }
-                return Math.floor(r * (end - begin + 1) + begin);
-            },
-            result = {};
-        result[now.format('yyyy-MM-dd')] = randomNum(10, 10000);
-        for (i = 1; i < 30; i++) {
-            now.setDate(now.getDate() - 1);
-            result[now.format('yyyy-MM-dd')] = randomNum(10, 10000);
-        }
-        return result;
-    };
-    consumeGrowth.setOption(getLineChartsOptions('充值趋势', '充值额度', _simulationData()));
-    payGrowth.setOption(getLineChartsOptions('消费趋势', '消费额度', _simulationData()));
+    $.get('assets/js/index.json', function(res) {
+            if (res.err_code == 0) {
+                consumeGrowth.setOption(getLineChartsOptions('充值趋势', '充值额度', res.consumeGrowth));
+                payGrowth.setOption(getLineChartsOptions('消费趋势', '消费额度', res.payGrowth));
+            }
+        })
+        // var _simulationData = function() {
+        //     Date.prototype.format = function(fmt) {
+        //         var o = {
+        //             'M+': this.getMonth() + 1, //月份 
+        //             'd+': this.getDate(), //日 
+        //             'h+': this.getHours(), //小时 
+        //             'm+': this.getMinutes(), //分 
+        //             's+': this.getSeconds(), //秒 
+        //             'q+': Math.floor((this.getMonth() + 3) / 3), //季度 
+        //             'S': this.getMilliseconds() //毫秒 
+        //         };
+        //         if (/(y+)/.test(fmt)) {
+        //             fmt = fmt.replace(RegExp.$1, (this.getFullYear() + '').substr(4 - RegExp.$1.length));
+        //         }
+        //         for (var k in o) {
+        //             if (new RegExp('(' + k + ')').test(fmt)) {
+        //                 fmt = fmt.replace(RegExp.$1, (RegExp.$1.length === 1) ? (o[k]) : (('00' + o[k]).substr(('' + o[k]).length)));
+        //             }
+        //         }
+        //         return fmt;
+        //     };
+        //     var now = new Date(),
+        //         randomNum = function(begin, end) {
+        //             begin = Number(begin);
+        //             end = Number(end);
+        //             var r = Math.random();
+        //             if (isNaN(begin) || isNaN(end) || begin > end) {
+        //                 return r;
+        //             }
+        //             return Math.floor(r * (end - begin + 1) + begin);
+        //         },
+        //         result = {};
+        //     result[now.format('yyyy-MM-dd')] = randomNum(10, 10000);
+        //     for (i = 1; i < 30; i++) {
+        //         now.setDate(now.getDate() - 1);
+        //         result[now.format('yyyy-MM-dd')] = randomNum(10, 10000);
+        //     }
+        //     return result;
+        // };
 });
